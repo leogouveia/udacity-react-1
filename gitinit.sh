5df5206eea179ccdb5438cb946d715f7a53c5514
Leonardo de Albuquerque Gouveia <leogouv@gmail.com>
2018-10-01 02:05:16 +0000

    Initial commit

---
+++ gitinit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ $# -lt 1 ];
+then
+  echo "Please, pass the name of the branch"
+  exit 1
+fi
+echo "node_modules " > ".gitignore"
+git init .
+git config user.email "leogouv@gmail.com"
+git config user.name "Leonardo de Albuquerque Gouveia"
+git remote add origin https://github.com/leogouveia/udacity-react-1.git
+git remote -v
+git checkout -b $1
+git add . 
+git commit -m "Initial commit"
+git push -u origin $1
